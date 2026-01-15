2170d1aa011b49382fd60656fbf5fa8607340b1e
PatRider64 <83703900+PatRider64@users.noreply.github.com>
2026-01-15 16:21:21 +0100

    Implémentation du déchiffrement de messages

--- main.py
+++ main.py
@@ -12,8 +12,30 @@ def encrypt_message(message):
 
     return encrypted_message, key, nonce
 
-message = sys.argv[1]
-encrypted_message, key, nonce = encrypt_message(message.encode())
-print(f"Message encrypté : {base64.b64encode(encrypted_message).decode()}")
-print(f"Clé : {base64.b64encode(key).decode()}")
-print(f"Nonce : {base64.b64encode(nonce).decode()}")
+def decrypt_message(message, key, nonce):
+    aesgcm = AESGCM(key)
+
+    decrypted_message = aesgcm.decrypt(nonce, message, None)
+
+    return decrypted_message
+
+def menu():
+    print("Choisissez une option :")
+    print("1 - Chiffrer un message")
+    print("2 - Déchiffrer un message")
+    return input("> ")
+
+choice = menu()
+
+if choice == "1":
+    message = input("Entrez le message à chiffrer : ").encode()
+    encrypted_message, key, nonce = encrypt_message(message)
+    print(f"Message encrypté : {base64.b64encode(encrypted_message).decode()}")
+    print(f"Clé : {base64.b64encode(key).decode()}")
+    print(f"Nonce : {base64.b64encode(nonce).decode()}")
+elif choice == "2":
+    message = base64.b64decode(input("Message chiffré : "))
+    key = base64.b64decode(input("Clé (Base64): "))
+    nonce = base64.b64decode(input("Nonce (Base64) : "))
+    decrypted_message = decrypt_message(message, key, nonce)
+    print(f"Message décrypté : {decrypted_message.decode()}")
